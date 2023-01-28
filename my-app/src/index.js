4f698f748c9817972dd93cdcbaa7852c8b1b6bf0
Antehich <westonefer@gmail.com>
2023-01-29 01:14:28 +0300

    Moving from class components to functions+hooks

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,29 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
-async function getAllQuestions(){
-    const response = await fetch('http://localhost:5500/api/appRequests/', {
-        method: 'GET'
-    })
-    if (response.ok) {
-       const json = await response.json()
-        console.log(json)
-        return json;
-    } else {
-        console.log('error')
-        return null
-    }
-}
-
-const allQuestions = getAllQuestions()
-
-
-
 class QuestionTemplate {
-    constructor(_id ,questionText, answerOption0, answerOption1, answerOption2, answerOption3, rightOption) {
-        this._id=_id;
+    constructor(_id, questionText, answerOption0, answerOption1, answerOption2, answerOption3, rightOption) {
+        this.id = _id;
         this.questionText = questionText;
         this.answerOption0 = answerOption0;
         this.answerOption1 = answerOption1;
@@ -32,51 +14,88 @@ class QuestionTemplate {
         this.rightOption = rightOption;
     }
 }
-function getRandomQuestion() {
+
+/*function getRandomQuestion() {
     const p = allQuestions[0]
-    console.log(p)
+    //console.log(p)
     //const q = new QuestionTemplate(p._id, p.questionText, p.answerOption0, p.answerOption1, p.answerOption2, p.answerOption3, p.rightOption)
     //console.log(q)
     //return q
-    //return new QuestionTemplate(allQuestions[Math.floor(Math.random()*allQuestions.length)])
-}
-getRandomQuestion()
+    return new QuestionTemplate(allQuestions[Math.floor(Math.random()*allQuestions.length)])
+}*/
 
-class Question extends React.Component{
-    render() {
-        return(
-            <li className="question" id="0">
-                <div className="questionText" id="0">{this.props.questionProps.questionText}</div>
-                <ol className="answerOptions">
-                    <li className="answerOption">{this.props.questionProps.answerOption0}</li>
-                    <li className="answerOption">{this.props.questionProps.answerOption1}</li>
-                    <li className="answerOption">{this.props.questionProps.answerOption2}</li>
-                    <li className="answerOption">{this.props.questionProps.answerOption3}</li>
-                </ol>
-            </li>
-        );
-    }
-}
+function Question({question}) {
 
-class QuestionList extends React.Component{
-    render() {
-        return(
-        <ol className="questionsBlock">
-            <Question questionProps={getRandomQuestion()}/>
-        </ol>
-        );
-    }
+    return (
+        <li className="question" id={question.id}>
+            <div className="questionText" id={question.id}>{question.questionText}</div>
+            <ol className="answerOptions" id={question.id}>
+                <li className="answerOption" id={question.id + 1} key={question.id + 1}>{question.answerOption0}</li>
+                <li className="answerOption" id={question.id + 2} key={question.id + 2}>{question.answerOption1}</li>
+                <li className="answerOption" id={question.id + 3} key={question.id + 3}>{question.answerOption2}</li>
+                <li className="answerOption" id={question.id + 4} key={question.id + 4}>{question.answerOption3}</li>
+            </ol>
+            <div>
+                <input type="text" name={"answer" + question.id} id={question.id} className="userAnswer"
+                       placeholder="Ваш ответ"></input>
+            </div>
+            <div className="checkedAnswer" name={"checkedAnswer" + question.id}></div>
+        </li>
+    );
 }
 
-class Test extends React.Component {
-    render() {
-        return (<QuestionList />)
+function App() {
+    const [availableQuestionsList, setAvailableQuestionsList] = React.useState([]);
+    const [randomQuestionsList, setRandomQuestionsList] = React.useState([]);
+    React.useEffect(() => {
+        async function getAllQuestions() {
+            const response = await fetch('http://localhost:5500/api/appRequests/', {
+                method: 'GET'
+            });
+            if (response.ok) {
+                const json = await response.json();
+                console.log(json);
+                setAvailableQuestionsList(json);
+                console.log(availableQuestionsList)
+            } else {
+                console.log('error')
+                setAvailableQuestionsList(['Server is not available!'])
+            }
+        }
+
+        getAllQuestions();
+    }, []);
+
+    function onClick() {
     }
+
+    React.useEffect(() => {
+        function getRandomQuestionsList() {
+            let randomChosenQuestionsList = []
+            for (let i = 1; i < 6; i++) {
+                let randomQuestionIndex = Math.floor(Math.random() * availableQuestionsList.length);
+                randomChosenQuestionsList.push(availableQuestionsList[randomQuestionIndex]);
+                availableQuestionsList.splice(randomQuestionIndex, 1);
+            }
+            //console.log(randomChosenQuestionsList)
+            setRandomQuestionsList(randomChosenQuestionsList);
+        }
+
+        getRandomQuestionsList();
+    }, []);
+
+
+    return (
+        randomQuestionsList.map((question) => (
+            <Question question={question}/>
+        ))
+    )
 }
+
 
 //========================================
 
 const test = ReactDOM.createRoot(document.getElementById("test"))
-//test.render(<Test />);
+test.render(<App/>);
 
 
